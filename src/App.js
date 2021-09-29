ed18018bdfb90f3e56139d39aaf62b74c1dcda66
Mathilde Legrand <m.legrand.duchaxel@gmail.com>
2021-09-29 21:52:35 +0200

    renew app file

--- src/App.js
+++ src/App.js
@@ -1,18 +1,14 @@
 import { BrowserRouter, Switch, Route } from "react-router-dom";
-import Register from "./components/Register";
-import Home from "./components/Home";
-import Login from "./components/Login";
-import Forum from "./components/Forum";
+import LandingPage from "./pages/LandingPage";
+import Login from "./pages/Login";
 
 function App() {
   return (
     <BrowserRouter>
       <div>
         <Switch>
-          <Route exact path="/" component= {Home} />
-          <Route exact path="/register" component= {Register} />
-          <Route exact path="/login" component= {Login} />
-          <Route exact path="/forum" component= {Forum} />
+          <Route exact path='/'><LandingPage /></Route>
+          <Route exact path='/login'><Login /></Route>
         </Switch>
       </div>
     </BrowserRouter>
