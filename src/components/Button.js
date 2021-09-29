1d50c5838a0e48fda2fef770f10034ee031eafac
Mathilde Legrand <m.legrand.duchaxel@gmail.com>
2021-09-29 21:52:22 +0200

    create components for buttons

---
+++ src/components/Button.js
@@ -0,0 +1,26 @@
+import React from 'react';
+
+export default class Button extends React.Component {
+
+    getColor = () => {
+        switch (this.props.color) {
+            case 'pink':
+                return 'bg-pink';
+            case 'red':
+                return 'bg-red';
+            default:
+                return 'bg-white';
+        }
+    }
+
+    render() {
+      const { text, ...otherProps } = this.props;
+
+      return (
+        <button className={`${this.getColor()} rounded-lg text-red text-center font-semibold px-5 py-2 m-2`} {...otherProps}>
+          {text}
+        </button>
+      );
+    }
+  }
+
