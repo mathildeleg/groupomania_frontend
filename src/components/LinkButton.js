7db79e96fdcec764e8091cdb55c7e59a382fc6c2
Mathilde Legrand <m.legrand.duchaxel@gmail.com>
2021-10-01 18:20:31 +0200

    format link buttons

--- src/components/LinkButton.js
+++ src/components/LinkButton.js
@@ -1,26 +1,27 @@
-import React from 'react';
-import { Link } from "react-router-dom";
+import React from 'react'
+import { Link } from 'react-router-dom'
 
 export default class LinkButton extends React.Component {
-
     getColor = () => {
         switch (this.props.color) {
             case 'pink':
-                return 'bg-pink';
+                return 'bg-pink'
             case 'red':
-                return 'bg-red';
+                return 'bg-red'
             default:
-                return 'bg-white';
+                return 'bg-white'
         }
     }
 
     render() {
-      const {to, text} = this.props;
-      return (
-        <Link to={to} className={`${this.getColor()} rounded-lg text-red text-center font-semibold px-5 py-2 m-2`}>
-          {text}
-        </Link>
-      );
+        const { to, text } = this.props
+        return (
+            <Link
+                to={to}
+                className={`${this.getColor()} rounded-lg text-red text-center font-semibold px-5 py-2 m-2`}
+            >
+                {text}
+            </Link>
+        )
     }
-  }
-
+}
