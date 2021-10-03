b5a70e44c49a190ca31d50c44319ea1c3dad2357
Mathilde Legrand <m.legrand.duchaxel@gmail.com>
2021-10-03 19:10:52 +0200

    add 1 post route

--- src/App.js
+++ src/App.js
@@ -3,9 +3,9 @@ import LandingPage from './pages/LandingPage'
 import Login from './pages/Login'
 import Forum from './pages/Forum'
 import Register from './pages/Register'
+import PostWithComments from './pages/PostWithComments'
 import { TokenProvider } from './helpers/TokenContext'
 import React from 'react'
-
 class App extends React.Component {
     render() {
         return (
@@ -26,8 +26,9 @@ class App extends React.Component {
                                 path="/register"
                                 component={Register}
                             ></Route>
-                            <Route exact path="/forum">
-                                <Forum />
+                            <Route exact path="/forum" component={Forum}>
+                            </Route>
+                            <Route exact path="/forum/post/:postId/comment" component={PostWithComments}>
                             </Route>
                         </TokenProvider>
                     </Switch>
