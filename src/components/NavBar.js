5bd1fa5866343f7acf712c315a5eec9662791c71
Mathilde Legrand <m.legrand.duchaxel@gmail.com>
2021-10-09 11:12:53 +0200

    modify design navbar

--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -4,8 +4,8 @@ import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
 import { faImages } from '@fortawesome/free-solid-svg-icons'
 import { faCog } from '@fortawesome/free-solid-svg-icons'
 import { faSignOutAlt } from '@fortawesome/free-solid-svg-icons'
-import { faArrowLeft } from '@fortawesome/free-solid-svg-icons'
-import { faArrowRight } from '@fortawesome/free-solid-svg-icons'
+import logo from '../icon.png'
+
 export default class NavBar extends React.Component {
     logOut = () => {
         localStorage.removeItem('token')
@@ -13,15 +13,15 @@ export default class NavBar extends React.Component {
 
     render() {
         return (
-            <div className="bg-white h-screen overflow-hidden m-3 pt-14 flex flex-col content-center w-8 rounded-lg">
-                {/* <img src='../src/icon.png' alt='logo'/> */}
+            <div className="bg-white h-screen overflow-hidden m-3 flex flex-col content-center w-8 rounded-lg">
+                <img src={logo} alt="logo" className="flex content-start mt-4" />
                 <Link to={'/forum'}>
                     <FontAwesomeIcon
                         icon={faImages}
-                        className="text-pink hover:text-red visited:text-red text-2xl m-0.5"
+                        className="text-pink hover:text-red visited:text-red text-2xl m-0.5 mt-16"
                     />
                 </Link>
-                <div className="mt-16">
+                <div className="mt-96">
                     <Link to={`/profile/me`}>
                         <FontAwesomeIcon
                             icon={faCog}
