e196d2367ce1a91c723aab0037240f079d505208
Mathilde Legrand <m.legrand.duchaxel@gmail.com>
2021-10-12 16:01:06 +0200

    improve design of comments

--- src/components/Comment.js
+++ src/components/Comment.js
@@ -1,25 +1,34 @@
 import React from 'react'
 import { timeAgo } from '../helpers/timeAgo'
-import { Link } from 'react-router-dom';
+import LinkButton from './LinkButton'
 
 export default class Comment extends React.Component {
     render() {
-        const { date, comment, author, commentId, postId } = this.props;
+        const { date, comment, author, commentId, postId } = this.props
         const minutes = timeAgo(date)
 
         return (
             <div className="flex bg-pink justify-end">
-            <Link to={`/forum/post/${postId}/comment/${commentId}`} className="container rounded-xl bg-white w-3/4 m-5 p-4">
-                <div className="flex flex-row justify-center p-3">
-                    <div className="flex pl-4">
-                        {comment}
+                <div className="container rounded-xl bg-white w-3/4 m-5 p-4">
+                <div className="flex flex-row justify-between content-start">
+                        <div className="flex flex-col justify-start pl-4">
+                            <div className="text-red">{author}</div>
+                            <div className="text-xs pl-8 lowercase">
+                                {minutes}
+                            </div>
+                        </div>
+                        <div>
+                            <LinkButton
+                                to={`/forum/post/${postId}/comment/${commentId}`}
+                                text="..."
+                                textColor="black"
+                            />
+                        </div>
+                    </div>
+                    <div className="flex flex-row justify-center p-3">
+                        <div className="flex pl-4">{comment}</div>
                     </div>
                 </div>
-                <div className="flex flex-row justify-between items-end">
-                    <div className="text-red">{author}</div>
-                    <div className="text-xs pl-8 lowercase">{minutes}</div>
-                </div>
-            </Link>
             </div>
         )
     }
