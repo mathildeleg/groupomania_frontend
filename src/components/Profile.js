8886f4434072325f90084c0896cccd822dae1f05
Mathilde Legrand <m.legrand.duchaxel@gmail.com>
2021-10-20 14:08:51 +0200

    remove button to modify avatar

--- src/components/Profile.js
+++ src/components/Profile.js
@@ -1,23 +1,17 @@
 import React from 'react'
-import AuthContext from '../helpers/AuthProvider'
 
 export default class Profile extends React.Component {
-    static contextType = AuthContext
-
     render() {
-        const { profile } = this.context
-        const { avatar, firstName, lastName, email, userId } = this.props
+        const { avatar, firstName, lastName, email } = this.props
 
         return (
             <div className="container flex flex-col bg-pink space-y-4">
-                {profile.userId === userId ? (
-                    <div className="flex flex-col space-x-4 justify-center p-4 m-8 space-y-4">
-                        <div>{avatar}</div>
-                        <button className="bg-white rounded-lg text-red font-semibold">
-                            Modifier la photo
-                        </button>
-                    </div>
-                ) : null}
+                <div className="flex flex-col space-x-4 justify-center p-4 m-8 space-y-4">
+                    <div>{avatar}</div>
+                    {/* <button className="bg-white rounded-lg text-red font-semibold">
+                        Modifier la photo
+                    </button> */}
+                </div>
                 <div className="space-y-4">
                     <div className="flex flex-col space-x-4 space-y-1 justify-center pr-5">
                         <div className="lowercase text-red font-semibold pl-4">
