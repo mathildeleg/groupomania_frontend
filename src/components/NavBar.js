91ecf2fdad49cf3527203f62fe209654e60a1e92
Mathilde Legrand <m.legrand.duchaxel@gmail.com>
2021-10-20 10:14:50 +0200

    design navbar modif

--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -13,25 +13,29 @@ export default class NavBar extends React.Component {
 
     render() {
         return (
-            <div className="bg-white h-screen overflow-hidden m-3 flex flex-col content-center w-8 rounded-lg">
-                <img src={logo} alt="logo" className="flex content-start mt-8" />
+            <div className="bg-white h-screen overflow-hidden m-3 flex flex-col content-center w-9 rounded-lg">
+                <img
+                    src={logo}
+                    alt="logo"
+                    className="flex content-start mt-8"
+                />
                 <Link to={'/forum'}>
                     <FontAwesomeIcon
                         icon={faImages}
-                        className="text-pink hover:text-red visited:text-red text-2xl m-0.5 mt-16"
+                        className="text-pink hover:text-red visited:text-red text-2xl flex m-1 mt-16"
                     />
                 </Link>
                 <div className="mt-96">
                     <Link to={`/profile/me`}>
                         <FontAwesomeIcon
                             icon={faCog}
-                            className="text-pink hover:text-red visited:text-red text-2xl m-1"
+                            className="text-pink hover:text-red visited:text-red text-2xl m-1.5"
                         />
                     </Link>
                     <button onClick={this.logOut}>
                         <FontAwesomeIcon
                             icon={faSignOutAlt}
-                            className="text-pink hover:text-red visited:text-red text-2xl m-1"
+                            className="text-pink hover:text-red visited:text-red text-2xl m-1.5"
                         />
                     </button>
                 </div>
