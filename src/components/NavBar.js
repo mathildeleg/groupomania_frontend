aa3031e02f164edb8818e4dde55fd9c76d0748b4
Mathilde Legrand <m.legrand.duchaxel@gmail.com>
2021-10-20 11:20:13 +0200

    add access to all profiles for admin through navbar

--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -4,14 +4,20 @@ import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
 import { faImages } from '@fortawesome/free-solid-svg-icons'
 import { faCog } from '@fortawesome/free-solid-svg-icons'
 import { faSignOutAlt } from '@fortawesome/free-solid-svg-icons'
+import { faUsersCog } from '@fortawesome/free-solid-svg-icons'
 import logo from '../icon.png'
+import AuthContext from '../helpers/AuthProvider'
 
 export default class NavBar extends React.Component {
+    static contextType = AuthContext
+
     logOut = () => {
         localStorage.removeItem('token')
     }
 
     render() {
+        const { profile } = this.context
+
         return (
             <div className="bg-white h-screen overflow-hidden m-3 flex flex-col content-center w-9 rounded-lg">
                 <img
@@ -26,6 +32,14 @@ export default class NavBar extends React.Component {
                     />
                 </Link>
                 <div className="mt-96">
+                    {profile.isAdmin === true ? (
+                        <Link to={`/profile`}>
+                            <FontAwesomeIcon
+                                icon={faUsersCog}
+                                className="text-pink hover:text-red visited:text-red text-2xl m-1 mb-16"
+                            />
+                        </Link>
+                    ) : null}
                     <Link to={`/profile/me`}>
                         <FontAwesomeIcon
                             icon={faCog}
