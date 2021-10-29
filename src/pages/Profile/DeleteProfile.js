5138e4382f676e3d8be99de8aae17037a53bda70
Mathilde Legrand <m.legrand.duchaxel@gmail.com>
2021-10-29 14:28:37 +0200

    upon deleting profile, refresh to landing page

--- src/pages/Profile/DeleteProfile.js
+++ src/pages/Profile/DeleteProfile.js
@@ -2,6 +2,7 @@ import React from 'react'
 import { customFetch } from '../../helpers/fetch'
 import { ClientURL } from '../../helpers/clientURL'
 import withPrivateRoute from '../../helpers/withPrivateRoute'
+import { Link } from 'react-router-dom'
 
 
 class DeleteProfile extends React.Component {
@@ -13,8 +14,10 @@ class DeleteProfile extends React.Component {
 
     render() {
         return (
-            <div className="rounded-lg flex justify-center"> 
+            <div className="rounded-lg flex justify-center">
+                <Link to='/'>
                 <button onClick={this.deleteProfile} className="bg-red dark:bg-red-dark rounded-xl text-white dark:text-pink-dark font-semibold p-2">Supprimer le compte</button>
+                </Link>
             </div>
         )
     }
